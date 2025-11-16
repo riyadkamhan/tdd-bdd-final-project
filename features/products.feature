aeecf44bdb4cb355a0c68c6a08d40c00a56c18a0
riyadhkamhan <riyadkamhan23@gmail.com>
2025-11-16 16:44:21 -0500

    Complete project: all BDD steps and features implemented

--- features/products.feature
+++ features/products.feature
@@ -1,40 +1,109 @@
-Feature: The product store service back-end
-    As a Product Store Owner
-    I need a RESTful catalog service
-    So that I can keep track of all my products
+Feature: Product Store Service UI
+  As a Product Store Owner
+  I want to manage my products via a UI
+  So that I can create, read, update, delete, and search products
 
 Background:
-    Given the following products
-        | name       | description     | price   | available | category   |
-        | Hat        | A red fedora    | 59.95   | True      | CLOTHS     |
-        | Shoes      | Blue shoes      | 120.50  | False     | CLOTHS     |
-        | Big Mac    | 1/4 lb burger   | 5.99    | True      | FOOD       |
-        | Sheets     | Full bed sheets | 87.00   | True      | HOUSEWARES |
+  Given the following products
+    | name       | description     | price  | available | category   |
+    | Hat        | A red fedora    | 59.95  | True      | CLOTHS     |
+    | Shoes      | Blue shoes      | 120.50 | False     | CLOTHS     |
+    | Big Mac    | 1/4 lb burger   | 5.99   | True      | FOOD       |
+    | Sheets     | Full bed sheets | 87.00  | True      | HOUSEWARES |
 
 Scenario: The server is running
-    When I visit the "Home Page"
-    Then I should see "Product Catalog Administration" in the title
-    And I should not see "404 Not Found"
+  When I visit the "Home Page"
+  Then I should see "Product Catalog Administration" in the title
+  And I should not see "404 Not Found"
 
 Scenario: Create a Product
-    When I visit the "Home Page"
-    And I set the "Name" to "Hammer"
-    And I set the "Description" to "Claw hammer"
-    And I select "True" in the "Available" dropdown
-    And I select "Tools" in the "Category" dropdown
-    And I set the "Price" to "34.95"
-    And I press the "Create" button
-    Then I should see the message "Success"
-    When I copy the "Id" field
-    And I press the "Clear" button
-    Then the "Id" field should be empty
-    And the "Name" field should be empty
-    And the "Description" field should be empty
-    When I paste the "Id" field
-    And I press the "Retrieve" button
-    Then I should see the message "Success"
-    And I should see "Hammer" in the "Name" field
-    And I should see "Claw hammer" in the "Description" field
-    And I should see "True" in the "Available" dropdown
-    And I should see "Tools" in the "Category" dropdown
-    And I should see "34.95" in the "Price" field
+  When I visit the "Home Page"
+  And I set the "Name" to "Hammer"
+  And I set the "Description" to "Claw hammer"
+  And I select "True" in the "Available" dropdown
+  And I select "Tools" in the "Category" dropdown
+  And I set the "Price" to "34.95"
+  And I press the "Create" button
+  Then I should see the message "Success"
+  When I copy the "Id" field
+  And I press the "Clear" button
+  Then the "Id" field should be empty
+  And the "Name" field should be empty
+  And the "Description" field should be empty
+  When I paste the "Id" field
+  And I press the "Retrieve" button
+  Then I should see the message "Success"
+  And I should see "Hammer" in the "Name" field
+  And I should see "Claw hammer" in the "Description" field
+  And I should see "True" in the "Available" dropdown
+  And I should see "Tools" in the "Category" dropdown
+  And I should see "34.95" in the "Price" field
+
+Scenario: Read a Product
+  When I visit the "Home Page"
+  And I set the "Name" to "Hat"
+  And I press the "Search" button
+  Then I should see the message "Success"
+  When I copy the "Id" field
+  And I press the "Clear" button
+  And I paste the "Id" field
+  And I press the "Retrieve" button
+  Then I should see the message "Success"
+  And I should see "Hat" in the "Name" field
+  And I should see "A red fedora" in the "Description" field
+  And I should see "True" in the "Available" dropdown
+  And I should see "CLOTHS" in the "Category" dropdown
+  And I should see "59.95" in the "Price" field
+
+Scenario: Update a Product
+  When I visit the "Home Page"
+  And I set the "Name" to "Shoes"
+  And I press the "Search" button
+  And I copy the "Id" field
+  And I press the "Clear" button
+  And I paste the "Id" field
+  And I set the "Description" to "Updated blue shoes"
+  And I set the "Price" to "130.00"
+  And I select "True" in the "Available" dropdown
+  And I press the "Update" button
+  Then I should see the message "Success"
+  And I should see "Updated blue shoes" in the "Description" field
+  And I should see "130.00" in the "Price" field
+
+Scenario: Delete a Product
+  When I visit the "Home Page"
+  And I set the "Name" to "Big Mac"
+  And I press the "Search" button
+  And I copy the "Id" field
+  And I press the "Clear" button
+  And I paste the "Id" field
+  And I press the "Delete" button
+  Then I should see the message "Success"
+
+Scenario: List all Products
+  When I visit the "Home Page"
+  And I press the "List All" button
+  Then I should see "Hat" in the list
+  And I should see "Shoes" in the list
+  And I should see "Sheets" in the list
+
+Scenario: Search Products by Category
+  When I visit the "Home Page"
+  And I select "CLOTHS" in the "Category" dropdown
+  And I press the "Search" button
+  Then I should see "Hat" in the list
+  And I should see "Shoes" in the list
+
+Scenario: Search Products by Availability
+  When I visit the "Home Page"
+  And I select "True" in the "Available" dropdown
+  And I press the "Search" button
+  Then I should see "Hat" in the list
+  And I should see "Sheets" in the list
+  And I should see "Big Mac" in the list
+
+Scenario: Search Products by Name
+  When I visit the "Home Page"
+  And I set the "Name" to "Sheets"
+  And I press the "Search" button
+  Then I should see "Sheets" in the list
--- service/routes.py
+++ service/routes.py
@@ -1,29 +1,20 @@
 ######################################################################
-# Copyright 2016, 2022 John J. Rofrano. All Rights Reserved.
+# Copyright 2016, 2023 John J. Rofrano. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
 # https://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 ######################################################################
-
-# spell: ignore Rofrano jsonify restx dbname
 """
 Product Store Service with UI
 """
-from flask import jsonify, request, abort
-from flask import url_for  # noqa: F401 pylint: disable=unused-import
-from service.models import Product
+from flask import jsonify, request, abort, url_for
+from service.models import Product, Category
 from service.common import status  # HTTP Status Codes
 from . import app
-
 
 ######################################################################
 # H E A L T H   C H E C K
@@ -33,7 +24,6 @@ def healthcheck():
     """Let them know our heart is still beating"""
     return jsonify(status=200, message="OK"), status.HTTP_200_OK
 
-
 ######################################################################
 # H O M E   P A G E
 ######################################################################
@@ -42,87 +32,103 @@ def index():
     """Base URL for our service"""
     return app.send_static_file("index.html")
 
-
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
 ######################################################################
 def check_content_type(content_type):
     """Checks that the media type is correct"""
     if "Content-Type" not in request.headers:
-        app.logger.error("No Content-Type specified.")
         abort(
             status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
             f"Content-Type must be {content_type}",
         )
-
-    if request.headers["Content-Type"] == content_type:
-        return
-
-    app.logger.error("Invalid Content-Type: %s", request.headers["Content-Type"])
-    abort(
-        status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
-        f"Content-Type must be {content_type}",
-    )
-
+    if request.headers["Content-Type"] != content_type:
+        abort(
+            status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+            f"Content-Type must be {content_type}",
+        )
 
 ######################################################################
 # C R E A T E   A   N E W   P R O D U C T
 ######################################################################
 @app.route("/products", methods=["POST"])
 def create_products():
-    """
-    Creates a Product
-    This endpoint will create a Product based the data in the body that is posted
-    """
-    app.logger.info("Request to Create a Product...")
+    """Creates a Product"""
     check_content_type("application/json")
-
     data = request.get_json()
-    app.logger.info("Processing: %s", data)
     product = Product()
     product.deserialize(data)
     product.create()
-    app.logger.info("Product with new id [%s] saved!", product.id)
-
-    message = product.serialize()
-
-    #
-    # Uncomment this line of code once you implement READ A PRODUCT
-    #
-    # location_url = url_for("get_products", product_id=product.id, _external=True)
-    location_url = "/"  # delete once READ is implemented
-    return jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
-
+    location_url = url_for("get_product", product_id=product.id, _external=True)
+    return jsonify(product.serialize()), status.HTTP_201_CREATED, {"Location": location_url}
 
 ######################################################################
 # L I S T   A L L   P R O D U C T S
 ######################################################################
+@app.route("/products", methods=["GET"])
+def list_products():
+    """List all Products or filter by query params"""
+    name = request.args.get("name")
+    category = request.args.get("category")
+    available = request.args.get("available")
 
-#
-# PLACE YOUR CODE TO LIST ALL PRODUCTS HERE
-#
+    products = Product.all()
+
+    # Filter by name
+    if name:
+        products = [p for p in products if p.name.lower() == name.lower()]
+
+    # Filter by category
+    if category:
+        try:
+            category_enum = Category[category.upper()]
+            products = [p for p in products if p.category == category_enum]
+        except KeyError:
+            products = []
+
+    # Filter by availability
+    if available is not None:
+        available_bool = available.lower() in ["true", "yes", "1"]
+        products = [p for p in products if p.available == available_bool]
+
+    return jsonify([p.serialize() for p in products]), status.HTTP_200_OK
 
 ######################################################################
 # R E A D   A   P R O D U C T
 ######################################################################
-
-#
-# PLACE YOUR CODE HERE TO READ A PRODUCT
-#
+@app.route("/products/<int:product_id>", methods=["GET"])
+def get_product(product_id):
+    """Retrieve a single Product"""
+    product = Product.find(product_id)
+    if not product:
+        abort(status.HTTP_404_NOT_FOUND, f"Product with id '{product_id}' was not found.")
+    return jsonify(product.serialize()), status.HTTP_200_OK
 
 ######################################################################
 # U P D A T E   A   P R O D U C T
 ######################################################################
+@app.route("/products/<int:product_id>", methods=["PUT"])
+def update_product(product_id):
+    """Update an existing Product"""
+    check_content_type("application/json")
+    product = Product.find(product_id)
+    if not product:
+        abort(status.HTTP_404_NOT_FOUND, f"Product with id '{product_id}' was not found.")
 
-#
-# PLACE YOUR CODE TO UPDATE A PRODUCT HERE
-#
+    data = request.get_json()
+    product.deserialize(data)
+    product.update()
+    return jsonify(product.serialize()), status.HTTP_200_OK
 
 ######################################################################
 # D E L E T E   A   P R O D U C T
 ######################################################################
-
-
-#
-# PLACE YOUR CODE TO DELETE A PRODUCT HERE
-#
+@app.route("/products/<int:product_id>", methods=["DELETE"])
+def delete_product(product_id):
+    """Delete a Product"""
+    product = Product.find(product_id)
+    if not product:
+        abort(status.HTTP_404_NOT_FOUND, f"Product with id '{product_id}' was not found.")
+    product.delete()
+    return "", status.HTTP_204_NO_CONTENT
+مس
